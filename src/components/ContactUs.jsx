b8722a728ce73afc708b6f4d11b50d3adca0941e
kamaludyn <kamaluddeen305@gmail.com>
2024-06-25 04:18:43 -0800

    minor fix

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -3,10 +3,21 @@ import { FaFacebookF, FaInstagram, FaPhone, FaEnvelope } from "react-icons/fa";
 const ContactUs = () => {
   return (
     <section className="contact-section">
-      <FaFacebookF className="icon" />
-      <FaInstagram className="icon" />
-      <FaPhone className="icon" />
-      <FaEnvelope className="icon" />
+      <a href="https://web.facebook.com/KingKamaludeen305/">
+        <FaFacebookF className="icon" />
+      </a>
+      <a href="https://www.instagram.com/kaydheen" target="_blank">
+        <FaInstagram className="icon" />
+      </a>
+      <a href="tel:+2348120657960" target="_blank">
+        <FaPhone className="icon" />
+      </a>
+      <a
+        href="mailto:Kamaludeen305@gmail.com?subject=Subject&body=Body%20text"
+        target="_blank"
+      >
+        <FaEnvelope className="icon" />
+      </a>
     </section>
   );
 };
